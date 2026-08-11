0d664af90c15c7462f2e994163ea0567d9e4d945
jjayashree680 <jjayashree680@gmail.com>
2026-08-11 14:24:02 +0000

    Updated query service

---
+++ src/api/v1/services/query_service.py
@@ -0,0 +1,8 @@
+from src.api.v1.agents.credit_card_agent import run_credit_card_agent
+
+# from src.api.v1.agents.agents import run_search_agent_stream
+
+
+def query_documents(query: str):
+    print(query)
+    return run_credit_card_agent(query)
