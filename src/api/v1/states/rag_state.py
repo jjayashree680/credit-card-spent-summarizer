ed3d77c5477079dba5e7f0758429d9fdec10aa92
unknown <fathi1712@gmail.com>
2026-08-18 17:37:06 +0000

    upload documents chnages added in streamlit

--- src/api/v1/states/rag_state.py
+++ src/api/v1/states/rag_state.py
@@ -4,26 +4,17 @@ from typing import TypedDict, Optional
 class RAGState(TypedDict, total=False):
 
     query: str
-
     card_id: Optional[str]
     billing_month: Optional[str]
-
     intent: dict
-
     user_name: Optional[str]
-
     sql_context: dict
-
     vector_docs: list
     fts_docs: list
     hybrid_docs: list
     reranked_docs: list
-
     final_context: str
-
     response: dict
-
     retry_count: int
     chat_history: list
-
     evaluation_result: dict
